3604565a7d63010cf7e84a0bf4d4b5d11dd44e04
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-17 12:18:54 -0700

    Create main1-part1.java

---
+++ main1-part1.java
@@ -0,0 +1,25 @@
+package Traning;
+import java.util.*;
+public class Jegan {
+
+	public static void main(String[] args) {
+		
+		int choice=0;
+		Scanner k=new Scanner(System.in);
+		Avg in=new Avg();
+		
+		System.out.printf("\nEnter 1  for add number:");
+		choice=k.nextInt();
+		
+		switch(choice)
+		{
+		case 1:
+		{
+			in.love();
+		}
+		}
+	  
+	   
+	}
+
+}
